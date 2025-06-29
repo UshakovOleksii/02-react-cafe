1bf77448b70ca57db08d3560b1f67d25106f5d56
UshakovOleksii <ushakovalex.05@gmail.com>
2025-06-29 16:23:00 +0200

    homework_2.1

---
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import css from './VoteOptions.module.css';
+import { VoteType } from '../../types/votes';
+
+interface VoteOptionsProps {
+  onVote: (type: VoteType) => void;
+  onReset: () => void;
+  canReset: boolean;
+}
+
+export default function VoteOptions({ onVote, onReset, canReset }: VoteOptionsProps) {
+  return (
+    <div className={css.container}>
+      {(['good', 'neutral', 'bad'] as VoteType[]).map(type => (
+        <button
+          key={type}
+          className={css.button}
+          onClick={() => onVote(type)}
+        >
+          {type[0].toUpperCase() + type.slice(1)}
+        </button>
+      ))}
+      {canReset && (
+        <button
+          className={`${css.button} ${css.reset}`}
+          onClick={onReset}
+        >
+          Reset
+        </button>
+      )}
+    </div>
+  );
+}
