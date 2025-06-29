3595d9b94d18c7956d58393ddd90270084ede94c
UshakovOleksii <ushakovalex.05@gmail.com>
2025-06-29 17:06:17 +0200

    homework_2.3

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,18 +1,23 @@
-import { useState } from 'react'
-import css from './App.module.css';     
-import CafeInfo from './components/CafeInfo/CafeInfo';
-import VoteOptions from './components/VoteOptions/VoteOptions';
-import VoteStats from './components/VoteStats/VoteStats';
-import Notification from './components/Notification/Notification';
-import type { Votes, VoteType } from './types/votes';
+import { useState } from 'react';
+import css from './App.module.css';
+
+import CafeInfo from '../CafeInfo/CafeInfo';
+import VoteOptions from '../VoteOptions/VoteOptions';
+import VoteStats from '../VoteStats/VoteStats';
+import Notification from '../Notification/Notification';
+
+import type { Votes, VoteType } from '../../types/votes';
 
 const initialVotes: Votes = { good: 0, neutral: 0, bad: 0 };
 
-function App() {
+export default function App() {
   const [votes, setVotes] = useState<Votes>(initialVotes);
 
   const handleVote = (type: VoteType) => {
-    setVotes(prev => ({ ...prev, [type]: prev[type] + 1 }));
+    setVotes((prev: Votes) => ({
+      ...prev,
+      [type]: prev[type] + 1,
+    }));
   };
 
   const resetVotes = () => setVotes(initialVotes);
@@ -30,7 +35,6 @@ function App() {
         onReset={resetVotes}
         canReset={totalVotes > 0}
       />
-
       {totalVotes > 0 ? (
         <VoteStats
           votes={votes}
@@ -43,6 +47,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
-
